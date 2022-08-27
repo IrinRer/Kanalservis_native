0d215c7a081a16fe84c2923acc6d4aa5e3947ab0
Irin <iraantonova1998@list.ru>
2022-08-27 09:55:59 +0300

    thunk for users

--- store/users/thunk.js
+++ store/users/thunk.js
@@ -1,1 +1,15 @@
-export const USERS_SLICE_ALIAS = 'users';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { api } from "../../network";
+import { USERS_SLICE_ALIAS } from "./constants";
+
+export const fetchUsersAction = createAsyncThunk(
+  `${USERS_SLICE_ALIAS}/fetch`,
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api().get("/users");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
