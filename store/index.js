a5e75af60f14b650457c04c657c3928bf8935a39
Irin <iraantonova1998@list.ru>
2022-08-27 11:20:44 +0300

    store for posts

--- store/index.js
+++ store/index.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthorizationReducer from "./auth/slice";
 import UsersReducer from "./users/slice";
+import PostsReducer from "./posts/slice";
 
 export const store = configureStore({
   reducer: {
     auth: AuthorizationReducer,
     users: UsersReducer,
-    // posts: PostsReducer,
-    // photos: PhotosReducer
+    posts: PostsReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
